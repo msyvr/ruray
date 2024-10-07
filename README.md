683091a1794045f337698f0d3a0f9a7b78e68fb9
msyvr <15218145+msyvr@users.noreply.github.com>
2024-10-07 15:58:21 -0700

    write image files; update README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-a ray tracer written in rust
+# a ray tracer written in rust
+
+## Project objective
+Basically, exercise my new Rust skills. Suggestions re making the code more idiomatic and performant are most welcome.
+
+### Goal 1
+Generate a photo-realistic image of a configured scene. 
+- Start by defining a `world` which includes both a `scene` (with lighting) and a `display` and a 'camera' `viewpoint`. 
+- Trace a `ray` that emanates from the `viewpoint`, passes through a `display` `pixel`, and interacts with the `scene`.
+  - Each interaction contributes to the final color value of that display `pixel`. 
+- Repeat for all `display` pixels. 
+- Save the display's pixel values (rgb tuples) to a (formatted) image file.
+
+From an [Nvidia ray tracing post](https://developer.nvidia.com/discover/ray-tracing), an overview of the conceptual layout of the 'world':
+!["ray tracing: conceptual layout, with viewpoint, display plane, and scene"](static/concept-ray-tracing.png)
+
+### Goal 2
+Optimize the code for speed.
+- Probably by implementing [Rayon](https://docs.rs/rayon/latest/rayon/).
+- Look into GPU acceleration (stretch).
+
+### Goal 3 (stretch)
+Create an animation.
+- Optimized code essential to be able to step through multiple real-time renders 'live'. 
+- Consider running on a cloud provider's more powerful machine (with GPU acceleration).
+
+### LICENSE
+Not having looked into licenses for a while, I did a quick search and found [this](https://www.reddit.com/r/opensource/comments/1b5oeq4/comment/kta5hwv/), which seemed to align with my intent - so, as reflected in the LICENSE file, Apache 2.0 it is.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
+use image;
+use std::fs::File;
+use std::path::Path;
+
+fn write_to_file(
+    img_filename: &str,
+    img_format: &str,
+    img_pixels: &[u8],
+    img_size:(usize, usize),
+) -> Result<(), std::io::Error> {
+
+    let format_ok = match img_format {
+        "jpeg" 
+        | "png" 
+        | "ico" 
+        | "pnm" 
+        | "bmp" 
+        | "exr" 
+        | "tiff" => true,
+        _ => false,
+
+    };
+
+    // TODO Improve error handling of unsupported formats. 
+    // Although... as it happens, not introducing a panic enabled
+    // serendipitous discovery of webp being supported by save_buffer!
+    if !format_ok {
+        println!("Unsupported image format. Expect an error for image::save_buffer.")
+    } 
+
+    let filename = [img_filename, ".", img_format].concat();
+    let path = Path::new(&filename);
+    File::create(path)?;
+    image::save_buffer(
+        path,
+        img_pixels,
+        img_size.0 as u32,
+        img_size.1 as u32,
+        image::ExtendedColorType::Rgb8,
+    ).unwrap();
+
+    Ok(())
+}
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn writes_single_pixel_to_new_image_file(){
+        let img_filename = "static/single_pixel";
+        let img_format = "png";
+        let img_pixels: [u8; 3] = [100, 10, 200];
+        let img_size:(usize, usize) = (1, 1);
+        let result = write_to_file(
+            img_filename,
+            img_format,
+            &img_pixels, 
+            img_size,
+        );
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn saves_as_webp(){
+        let img_filename = "static/wrong_format";
+        let img_format = "webp";
+        let img_pixels: [u8; 3] = [100, 10, 200];
+        let img_size:(usize, usize) = (1, 1);
+        let result = write_to_file(
+            img_filename,
+            img_format,
+            &img_pixels,
+            img_size,
+        );
+
+        assert!(result.is_ok());
+        // Ummm - this seems to work, contrary to the docs?
+        // Oh, and check it out! png 72 bytes, webp 36 bytes. Whoa!
+    }    
+    
+}
+
 fn main() {
     println!("Hello, world! Welcome to ruray - my rust ray tracer.");
+
 }
